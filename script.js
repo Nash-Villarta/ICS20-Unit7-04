59ddfc920b568406bcaa180f19603d9786dcd823
NashVillarta <>
2021-05-07 03:10:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+document.getElementById('button').addEventListener('click', findprice)
+let userage = 0
+
+function findprice () {
+  userage = document.getElementById('inputnumber').value
+  userday = document.getElementById('inputtime').value
+  userage = parseInt(userage)
+//  && is used to seperate identical variables, || is used for seperate different variables. It must be Wednesday or Saturday and Ages 4-21 to get discount. Else would result in a price of $50.
+  if (userday == 'Wednesday' && userage > 4 && userage < 21 || userday == 'Saturday' && userage > 4 && userage < 21) {
+    document.getElementById('textbox').innerHTML = 'You can get a student admission of $12'
+  } else {
+    document.getElementById('textbox').innerHTML = 'You have the regular admission of $50'
+  }
+}
